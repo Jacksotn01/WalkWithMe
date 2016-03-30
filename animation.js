9edd7ae8ad1329098b45b56b4490b3d70f7fbcca
Jacksotn01 <jacksotn01@gmail.com>
2016-03-29 20:59:08 -0400

    First Commit

---
+++ animation.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//   $(".plane").fadeIn(15000, function(){
+//     $(".plane").animate({
+//       marginLeft:"+=550px"
+//     },15000,function(){
+//       $(".plane").fadeOut(1000);
+//     });
+//   });
+// });
