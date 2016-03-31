5655d43bce90089c2806205c6c4c53861b00e8cf
Jacksotn01 <jacksotn01@gmail.com>
2016-03-31 18:20:35 -0400

    Final Changes

--- animation.js
+++ animation.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
-  $(".plane").fadeIn(15000, function(){
-    $(".plane").animate({
-      marginLeft:"+=550px"
-    },15000,function(){
-      $(".plane").fadeOut(1000);
-    });
-  });
+  $(".thailand").mouseenter( function () {
+    $('.hover').css("display", "block");
+  }).mouseleave(function () {
+    $('.hover').css("display", "none");
+
+  $(".cambodia").mouseenter( function () {
+    $('.hover2').css("display", "block");
+  }).mouseleave(function () {
+    $('.hover2').css("display", "none");
+
+    $(".singapore").mouseenter( function () {
+    $('.hover3').css("display", "block");
+  }).mouseleave(function () {
+    $('.hover3').css("display", "none");
+
+    $(".peru").mouseenter( function () {
+    $('.hover4').css("display", "block");
+  }).mouseleave(function () {
+    $('.hover4').css("display", "none");
+
+     $(".bolivia").mouseenter( function () {
+    $('.hover5').css("display", "block");
+  }).mouseleave(function () {
+    $('.hover5').css("display", "none");
+
+     $(".ecuador").mouseenter( function () {
+    $('.hover6').css("display", "block");
+  }).mouseleave(function () {
+    $('.hover6').css("display", "none");
+            })
+          })
+        })
+      })
+    })
+  })
 });
